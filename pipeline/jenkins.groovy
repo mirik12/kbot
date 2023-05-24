71898989d74c73f3230ea842400eb8c6170085e0
mirik12 <39529598+mirik12@users.noreply.github.com>
2023-05-24 11:48:41 +0200

    Update jenkins.groovy

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -1,17 +1,48 @@
 pipeline {
-    agent any
-    parameters {
-
-        choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
-
+agent any
+    environment {
+        REPO = "https://github.com/mirik12/kbot"
+        BRANCH = 'main'
     }
     stages {
-        stage('Example') {
+
+        stage ("clone") {
             steps {
-                echo "Build for platform ${params.OS}"
+            echo 'CLONE REPOSITORY'
+                git branch: "${BRANCH}", url: "${REPO}"
+            }
+       }
 
-                echo "Build for arch: ${params.ARCH}"
+        stage ("test") {
+            steps {
+                echo 'TEST EXECUTION STARTED '
+                sh 'make test'
+            }
+       }
 
+        stage ("build") {
+            steps {
+                echo 'BUILD EXECUTION STARTED '
+                sh 'make build'
+            }
+       }
+
+        stage ("image") {
+            steps {
+                script {
+                    echo 'TEST EXECUTION STARTED '
+                    sh 'make image'
+                }
+            }
+       }
+
+        stage ("push") {
+            steps {
+                script {
+                    docker.withRegistry ( '', 'dockerhub ') {
+                    sh 'make push'
+                    }
+                }
             }
         }
     }
